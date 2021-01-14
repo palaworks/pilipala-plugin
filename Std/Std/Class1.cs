38baef707c66ffaea5739ce26e18c8e0c06d905c
Thaumy Cheng <Studio_ai@outlook.com>
2021-01-14 17:58:30 +0800

    ~~

--- Std/Std/Class1.cs
+++ Std/Std/Class1.cs
@@ -2,7 +2,37 @@
 
 namespace Std
 {
-    public class Class1
+    public class StdCheck
     {
+        /// <summary>
+        /// 交换值的方法，引用类型
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        public static void Exch<T>(ref T i, ref T j)
+        {
+            T temp = i;
+            i = j;
+            j = temp;
+        }
+        /// <summary>
+        /// 判断小于的方法
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns>若i小于j返回true</returns>
+        public static bool Less<T>(T i, T j) where T : IComparable
+        {
+            return i.CompareTo(j) < 0;
+        }
+        /// <summary>
+        /// 判断奇数的方法
+        /// </summary>
+        /// <param name="num">待判断的数值</param>
+        /// <returns>num为奇数返回true，num为偶数返回false</returns>
+        public static bool IsOdd(int num)
+        {
+            return (num % 2) == 1;
+        }
     }
 }
