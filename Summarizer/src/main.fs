77bc80cf42cc9a130682f698f085627c9594afb7
Thaumy <thaumy@outlook.com>
2022-08-08 08:45:48 +0800

    Update main.fs

--- Summarizer/src/main.fs
+++ Summarizer/src/main.fs
@@ -5,35 +5,36 @@ open fsharper.op
 open fsharper.typ
 open fsharper.op.Alias
 open DbManaged.PgSql
+open pilipala.container.post
 open pilipala.plugin
 open pilipala.data.db
 open pilipala.pipeline
 open pilipala.util.text
 open pilipala.pipeline.post
 
-type Summarizer(renderBuilder: IPostRenderPipelineBuilder, db: IDbOperationBuilder, cfg: IPluginCfgProvider) =
+type Summarizer
+    (
+        postRenderBuilder: IPostRenderPipelineBuilder,
+        mappedPostProvider: IMappedPostProvider,
+        cfg: IPluginCfgProvider
+    ) =
 
     let summaries =
         { json = cfg.config }
             .deserializeTo<Dictionary<u64, string>> ()
 
-    let getBody id : string =
-        db
-            .makeCmd()
-            .getFstVal (db.tables.post, "post_body", "post_id", id)
-        |> db.managed.executeQuery
-        |> fmap coerce
-        |> unwrap
+    let getBody id =
+        mappedPostProvider.fetch
 
-    do
-        let f id : u64 * obj =
-            match summaries.TryGetValue(id).intoOption' () with
-            | None ->
-                id,
-                { html = getBody id }
-                    .withoutTags()
-                    .Substring(0, 80)
-            | Some x -> (id, x)
+        do
+            let f id : u64 * obj =
+                match summaries.TryGetValue(id).intoOption' () with
+                | None ->
+                    id,
+                    { html = getBody id }
+                        .withoutTags()
+                        .Substring(0, 80)
+                | Some x -> (id, x)
 
-        renderBuilder.["Summary"].collection.Add
-        <| Replace(fun _ -> f)
+            renderBuilder.["Summary"].collection.Add
+            <| Replace(fun _ -> f)
