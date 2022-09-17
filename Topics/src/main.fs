fbea706541c3770026929ac3ffa3bcf40bc22a29
Thaumy <thaumy@outlook.com>
2022-09-17 10:36:04 +0800

    add Topics plugin

---
+++ Topics/src/main.fs
@@ -0,0 +1,29 @@
+﻿namespace pilipala.plugin
+
+open System
+open System.Collections.Generic
+open fsharper.op
+open fsharper.typ
+open fsharper.alias
+open pilipala.plugin
+open pilipala.pipeline
+open pilipala.util.text
+open pilipala.pipeline.post
+
+type Topics(postRenderBuilder: IPostRenderPipelineBuilder, cfg: IPluginCfgProvider) =
+
+    let topics =
+        { json = cfg.config }
+            .deserializeTo<Dictionary<i64, string []>> ()
+
+    do
+        let f id : i64 * obj =
+            id,
+            topics
+                .TryGetValue(id)
+                .intoOption'()
+                .unwrapOr(fun _ -> [||])
+                .obj ()
+
+        postRenderBuilder.["Topics"].collection.Add
+        <| Replace(always f)
