eb2ba9806db8f20d7e1b9ec39d4e29679a3a9942
Thaumy <thaumy@outlook.com>
2022-09-22 11:49:01 +0800

    add PartialOrder

---
+++ PartialOrder/src/post.fs
@@ -0,0 +1,46 @@
+module internal forPost
+
+open fsharper.typ
+open pilipala.data.db
+open pilipala.pipeline
+open pilipala.pipeline.post
+
+let forPost (renderBuilder: IPostRenderPipelineBuilder) (db: IDbOperationBuilder) =
+
+    do
+        let f post_id =
+            post_id,
+            db {
+                getFstVal
+                    (db.managed.normalizeSql
+                        $"SELECT post_id FROM {db.tables.post} \
+                          WHERE post_id < <post_id> \
+                          ORDER BY post_id DESC \
+                          LIMIT 1")
+                    [ ("post_id", post_id) ]
+
+                execute
+            }
+            :> obj
+
+        renderBuilder.["PredId"].collection.Add
+        <| Replace(fun _ -> f)
+
+    do
+        let f post_id =
+            post_id,
+            db {
+                getFstVal
+                    (db.managed.normalizeSql
+                        $"SELECT post_id FROM {db.tables.post} \
+                          WHERE post_id > <post_id> \
+                          ORDER BY post_id \
+                          LIMIT 1")
+                    [ ("post_id", post_id) ]
+
+                execute
+            }
+            :> obj
+
+        renderBuilder.["SuccId"].collection.Add
+        <| Replace(fun _ -> f)
