5094851d2a7a1093688ad73d87dad3153cf6703a
Thaumy <thaumy@outlook.com>
2022-09-22 21:40:48 +0800

    add UserAvatarUrl plugin and UserSiteUrl plugin

---
+++ UserAvatarUrl/src/main.fs
@@ -0,0 +1,28 @@
+﻿namespace pilipala.plugin
+
+open fsharper.op
+open fsharper.typ
+open fsharper.alias
+open pilipala.pipeline
+open pilipala.pipeline.comment
+open pilipala.util.text
+open pilipala.pipeline.post
+open forPost
+open forComment
+
+type UserAvatarUrl
+    (
+        postRenderBuilder: IPostRenderPipelineBuilder,
+        postRenderPipeline: IPostRenderPipeline,
+        commentRenderBuilder: ICommentRenderPipelineBuilder,
+        commentRenderPipeline: ICommentRenderPipeline,
+        cfg: IPluginCfgProvider
+    ) =
+
+    let map =
+        { json = cfg.config }
+            .deserializeTo<Dict<i64, string>> ()
+
+    do
+        forPost postRenderBuilder postRenderPipeline map
+        forComment commentRenderBuilder commentRenderPipeline map
