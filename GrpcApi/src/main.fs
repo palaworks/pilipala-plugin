a77d00d4aecda8422766c3a1facf2be8533760eb
Thaumy <thaumy@outlook.com>
2023-01-05 20:25:20 +0800

    init GrpcApi plugin template

---
+++ GrpcApi/src/main.fs
@@ -0,0 +1,18 @@
+﻿namespace pilipala.plugin
+
+open pilipala
+open fsharper.op
+open fsharper.typ
+open pilipala.plugin
+open pilipala.util.text
+open plugin.cfg
+
+[<HookOn(AppLifeCycle.AfterBuild)>]
+type GrpcApi(cfg: IPluginCfgProvider, app: IApp) =
+    do
+        let cfg =
+            { json = cfg.config }
+                .deserializeTo<Cfg>()
+                .unwrap ()
+
+        ()
---
+++ GrpcApi/src/plugin/cfg.fs
@@ -0,0 +1,11 @@
+module plugin.cfg
+
+open fsharper.alias
+
+type Cfg =
+    { a: string
+      b: string
+      c: string
+      d: string
+      e: string
+      f: string }
