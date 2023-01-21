14ffe1528698dead0c2dc10c885f920467fbae28
Thaumy <thaumy@outlook.com>
2023-01-21 11:18:57 +0800

    + log for host starting

--- WsApi/src/plugin/hosting.fs
+++ WsApi/src/plugin/hosting.fs
@@ -3,23 +3,27 @@ module plugin.hosting
 open System.Threading.Tasks
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
 open fsharper.typ
 open WebSocketSharp.Server
 
-type Worker(wsLocalServer: WebSocketServer, wsPublicServer: WebSocketServer) =
+type Worker(wsLocalServer: WebSocketServer, wsPublicServer: WebSocketServer, logger: ILogger) =
     inherit BackgroundService()
 
     override self.ExecuteAsync _ =
         fun _ ->
             wsLocalServer.Start()
             wsPublicServer.Start()
+
+            logger.LogInformation $"Local WebSocket now listening on {wsLocalServer.Address}:{wsLocalServer.Port}"
+            logger.LogInformation $"Public WebSocket now listening on {wsPublicServer.Address}:{wsPublicServer.Port}"
         |> Task.RunAsTask
 
-let runHost (wsLocalServer, wsPublicServer) =
+let runHost wsLocalServer wsPublicServer (logger: ILogger) =
     Host
         .CreateDefaultBuilder()
         .ConfigureServices(fun ctx services ->
-            fun _ -> new Worker(wsLocalServer, wsPublicServer)
+            fun _ -> new Worker(wsLocalServer, wsPublicServer, logger)
             |> services.AddHostedService
             |> ignore)
         .Build()
