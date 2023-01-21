9cb74990674ad48b459d73a0da5ac9b1cbc85c69
Thaumy <thaumy@outlook.com>
2023-01-21 20:30:26 +0800

    fix code

--- WsApi/src/main.fs
+++ WsApi/src/main.fs
@@ -15,6 +15,27 @@ open plugin.hosting
 type WsApi(cfg: IPluginCfgProvider, app: IApp, logger: ILogger<WsApi>) =
     do
         let cfg = { json = cfg.config }.deserializeTo<Cfg>().unwrap ()
+        
+        //Display
+        let pl_display_user =
+            app.userLoginByName cfg.pl_comment_user cfg.pl_comment_pwd |> unwrap
 
+        //Anonymous
+        let pl_comment_user =
+            app.userLoginByName cfg.pl_comment_user cfg.pl_comment_pwd |> unwrap
+
+        let wsLocalServer =
+            (wsLocalServer cfg.ws_local_port).configRouting
+            <| (pl_display_user, pl_comment_user, cfg.enable_api_response_detail_logging, logger)
+
+        let wsPublicServer =
+            let cert_path =
+                if cfg.ws_public_enable_ssl then
+                    Some(cfg.ws_cert_pem_path, cfg.ws_cert_key_path)
+                else
+                    None
+
+            (wsPublicServer cfg.ws_public_port cert_path).configRouting
+            <| (pl_display_user, pl_comment_user, cfg.enable_api_response_detail_logging, logger)
 
         runHost wsLocalServer wsPublicServer logger
